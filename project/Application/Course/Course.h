dcea395a161e76ec458bb5df946c5c2e688b67e9
HayatoAbe2 <k024g0033@g.neec.ac.jp>
2026-02-27 14:09:42 +0900

    壁とその判定を追加

--- project/Application/Course/Course.h
+++ project/Application/Course/Course.h
@@ -2,6 +2,7 @@
 #include "GameEngine.h"
 #include "Ring/Ring.h"
 #include "Spike/Spike.h"
+#include "OBB.h"
 
 class Course {
 public:
@@ -16,9 +17,13 @@ public:
 	// 描画
 	void Draw(const std::shared_ptr<DirectionalLight> directionalLight);
 
+	// 衝突時
+	void OnCollide();
+
 	// Getter
 	Vector2 GetColliderCenter() { return colliderCenter_; }
-	float GetColliderRadius() { return colliderRadius_; }
+	float GetColliderRadius() { return radius_; }
+	std::vector<OBB> GetWalls() { return walls_; }
 
 	std::vector<Ring*> GetRings() {
 		std::vector<Ring*> rings;
@@ -37,9 +42,15 @@ public:
 	}
 
 private:
+	Vector3 GetPoint(float t);
+	Vector3 CatmullRomTangent(const Vector3& p0, const Vector3& p1, const Vector3& p2, const Vector3& p3, float t);
+	Vector3 GetTangent(float t);
+	Quaternion FromRotationMatrix(const Matrix3x3& m);
+	void CreateTubeCourse();
+
 	// 衝突判定(円)
 	Vector2 colliderCenter_ = {};
-	float colliderRadius_ = 10;
+	float radius_ = 10;
 
 	// 配置物
 	int ringCount_ = 20;
@@ -48,9 +59,17 @@ private:
 	std::vector<std::unique_ptr<Spike>> spikes_;
 
 	// モデル
-	std::unique_ptr<Object> model_;
+	std::vector<std::unique_ptr<Object>> wallModel_;
+
 	// トランスフォーム
 	SRT transform_;
+
+	// 板を並べて円形にする
+	std::vector<OBB> walls_;
+	const int wallCount_ = 32;
+	Vector3 wallSize_ = { 1.0f,0.75f,0.1f };
+	const float wallSpace_ = 1.5f;
+	std::vector<Vector3> controlPoints_;
 };
 
 
