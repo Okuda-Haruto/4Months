40c253611cc3b13c61658be28477ccec335b3009
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2026-02-28 16:58:46 +0900

    一番近いリングに向かうように

--- project/Application/Course/Course.cpp
+++ project/Application/Course/Course.cpp
@@ -25,6 +25,12 @@ void Course::Initialize() {
 	for (auto& ring : rings_) {
 		ring->Initialize();
 	}
+	//ソート
+	std::sort(rings_.begin(), rings_.end(),
+		[](const auto& a, const auto& b)
+		{
+			return *a < *b;
+		});
 
 	for (auto& spike : spikes_) {
 		spike->Initialize();
@@ -70,7 +76,7 @@ void Course::Draw(const std::shared_ptr<DirectionalLight> directionalLight) {
 	for (std::unique_ptr<Object>& model : wallModel_) {
 		objects.push_back(model.get());
 	}
-	Object::InstancingDraw3D(objects, directionalLight, nullptr, nullptr);
+	//Object::InstancingDraw3D(objects, directionalLight, nullptr, nullptr);
 }
 
 void Course::OnCollide() {
