c83f2c9814cd3a05c4d631df6decdadff6d9f9c3
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2026-02-28 17:18:25 +0900

    帰る

--- project/Application/Human/Enemy/Enemy.cpp
+++ project/Application/Human/Enemy/Enemy.cpp
@@ -64,7 +64,46 @@ void Enemy::Update() {
 		}
 
 	} else {
+		int index = -1;
+		float length = -1;
 
+		//近いリングを参照
+		for (int i = int(rings_.size()) - 1; i >= 0; i--) {
+			//リング位置
+			Vector3 center = rings_[i]->GetColliderCenter();
+
+			if (center.y > transform_.translate.y) {
+
+				//目標地点に向かう距離
+				Vector3 toTarget = center - transform_.translate;
+
+				//一番近いリングを捜索
+				if (length == -1 || (length > Length(toTarget))) {
+					index = i;
+					length = Length(toTarget);
+				}
+			}
+		}
+
+		//とりあえず愚直に向かうように
+		if (index != -1) {
+
+			//目標地点に向かう距離
+			Vector3 toTarget = rings_[index]->GetColliderCenter() - transform_.translate;
+
+			if (toTarget.x > 0.2f) {
+				NextRotate = NextRotate * MakeRotateAxisAngleQuaternion(Vector3{ 0,1,0 } *rotateMatrix, -std::numbers::pi_v<float> / 4);
+			}
+			if (toTarget.x < -0.2f) {
+				NextRotate = NextRotate * MakeRotateAxisAngleQuaternion(Vector3{ 0,1,0 } *rotateMatrix, std::numbers::pi_v<float> / 4);
+			}
+			if (toTarget.z > 0.2f) {
+				NextRotate = NextRotate * MakeRotateAxisAngleQuaternion(Vector3{ 1,0,0 } *rotateMatrix, -std::numbers::pi_v<float> / 4);
+			}
+			if (toTarget.z < -0.2f) {
+				NextRotate = NextRotate * MakeRotateAxisAngleQuaternion(Vector3{ 1,0,0 } *rotateMatrix, std::numbers::pi_v<float> / 4);
+			}
+		}
 	}
 
 	//現在の向きと次の向きの補完
