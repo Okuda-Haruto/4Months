fc17b863b602212779697c7dce2885552fd74452
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2026-03-02 03:23:36 +0900

    rise

--- project/main.cpp
+++ project/main.cpp
@@ -8,14 +8,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//ゲームエンジン
 	GameEngine::Initialize(L"LE2A_03_オクダ_ハルト", 1280, 720);
 
+	//一旦
+	shared_ptr<Input> input = make_shared<Input>();
+	input->Initialize(GameEngine::GetWindowsAPI());
+
 	//unique_ptr<SampleScene> gameManager = make_unique<SampleScene>();
 	//gameManager->Initialize();
 	unique_ptr<GameScene> gameScene = make_unique<GameScene>();
-	gameScene->Initialize();
+	gameScene->Initialize(input);
 
 	//ウィンドウの×ボタンが押されるまでループ
 	while (GameEngine::WindowState()) {
 		if (GameEngine::StartFlame()) {
+
+			//入力処理
+			input->Update();
 
 			//
 			//	更新処理
@@ -35,6 +42,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 			GameEngine::PostDraw();
 
+
+			if (input->PushKey(DIK_R)) {
+				gameScene.reset();
+				gameScene = make_unique<GameScene>();
+				gameScene->Initialize(input);
+			}
 		}
 	}
 
