13846e9856d47a961510352cc4f213817df8ac68
HayatoAbe2 <k024g0033@g.neec.ac.jp>
2026-03-02 18:18:12 +0900

    a

--- project/Application/Course/Course.cpp
+++ project/Application/Course/Course.cpp
@@ -114,14 +114,14 @@ void Course::Draw(const std::shared_ptr<DirectionalLight> directionalLight) {
 	for (auto& spike : spikes_) {
 		spike->Draw(directionalLight);
 	}
-
+	model_->SetDirectionalLight(directionalLight);
 	model_->Draw3D();
 
 	std::list<Object*> objects;
 	for (std::unique_ptr<Object>& model : wallModel_) {
 		objects.push_back(model.get());
 	}
-	Object::InstancingDraw3D(objects, directionalLight, nullptr, nullptr);
+	//Object::InstancingDraw3D(objects, directionalLight, nullptr, nullptr);
 }
 
 void Course::OnCollide() {
